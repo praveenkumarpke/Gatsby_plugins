e2bc066a3f20c7d216cf039586e953d097f053ac
praveenkumarpke <praveenpke@gmail.com>
2022-05-23 16:34:25 +0530

    Static image - plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,4 +3,13 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [],
-}
+};
+module.exports = {
+  siteMetadata: {
+    title: "My First Gatsby Site",
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { StaticImage } from "gatsby-plugin-image"
 
 // styles
 const pageStyles = {
@@ -86,6 +87,7 @@ const links = [
     description:
       "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
     color: "#E95800",
+    border: "10px solid #E95800"
   },
   {
     text: "How to Guides",
@@ -155,7 +157,7 @@ const IndexPage = () => {
           </a>
         </li>
         {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
+          <li key={link.url} style={{ ...listItemStyles, color: link.color, border: link.border, padding: "10px" }}>
             <span>
               <a
                 style={linkStyle}
@@ -177,7 +179,12 @@ const IndexPage = () => {
         alt="Gatsby G Logo"
         src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
       />
+      <h1>Static Image</h1>
+      <StaticImage alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"></StaticImage>
+        <h1>Dynamic Image</h1>
     </main>
+    
   )
 }
 
